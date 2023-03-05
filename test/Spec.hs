eaafd07fac2b3f5a6d24c0abdb9f049f9c457631
Jamie Fox <foxjamiek@gmail.com>
2023-03-05 22:26:58 +0100

    Add positive tests for parsePrefixTerm

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,16 @@
 import Test.HUnit
 import Test.Parse.Rule (ruleTests)
-import Test.Parse.Term (termTests)
+import Test.Parse.Term (termTests, prefixTermTests)
 import Test.Parse.Trs.Cops (parseCopsTests)
 import Test.Parse.Trs.Sig (sigTests)
 
 -- | The testing entry point. Runs each test in turn and logs output to the console.
 main :: IO ()
 main = do
-  putStrLn "Testing term parsing"
+  putStrLn "Testing term parsing in applicative format"
   _ <- runTestTT termTests
+  putStrLn "Testing term parsing in prefix format"
+  _ <- runTestTT  prefixTermTests
   putStrLn "Testing rule parsing"
   _ <- runTestTT ruleTests
   putStrLn "Testing signature parsing"
