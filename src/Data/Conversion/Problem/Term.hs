2c22e2f804ac3ac4aa5746e58791b1b3aeb017ee
Jamie Fox <foxjamiek@gmail.com>
2023-03-05 14:09:01 +0100

    Remove term-rewriting dependency

--- src/Data/Conversion/Problem/Term.hs
+++ src/Data/Conversion/Problem/Term.hs
@@ -1,20 +1,21 @@
 module Data.Conversion.Problem.Term
   ( Term (..),
-    foldTerm,
     termFunArities,
   )
 where
 
 import Data.Conversion.Problem.Trs.Sig (Sig (..), checkConsistentSig)
 import Data.List (nub)
-import Data.Rewriting.Term.Type (Term (..), fold)
 import Prettyprinter (Pretty, comma, encloseSep, lparen, pretty, rparen)
 
--- | Fold terms
---
--- qqjf
-foldTerm :: (v -> a) -> (f -> [a] -> a) -> Term f v -> a
-foldTerm = fold
+-- | The type for a term with function symbol type @f@ and variable type @v@.
+-- e.g. @g(x)@ might correspond to @Fun "g" [Var "x"]@ if @f@ and @v@ are strings
+data Term f v
+  = -- | 'Fun' represents a function application to a list of arguments
+    Fun f [Term f v]
+  | -- | 'Var' us used for a single variable
+    Var v
+  deriving (Show, Eq, Ord)
 
 -- | Return a list of function symbols and arities
 -- Removes duplicates and asserts that each function symbol name appears at most once
@@ -22,6 +23,10 @@ foldTerm = fold
 termFunArities :: (Eq f, Show f) => Term f v -> Either String [Sig f]
 termFunArities t = checkConsistentSig $ nub arities
   where
+    -- \| Recursively get arities of each function symbol in term. Logic copied from Haskell package 'Data.Rewriting.Term.Type'
+    foldTerm :: (v -> a) -> (f -> [a] -> a) -> Term f v -> a
+    foldTerm var _ (Var v) = var v
+    foldTerm var fun (Fun f ts) = fun f (fmap (foldTerm var fun) ts)
     arities = foldTerm (const id) (\f xs -> (Sig f (length xs) :) . foldr (.) id xs) t []
 
 -- | Make 'Term' an instance of 'Pretty'
