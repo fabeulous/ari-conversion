17f77cb1bb12d17228dc46c88e74f4f4ab59db1b
Jamie Fox <foxjamiek@gmail.com>
2023-03-06 21:29:04 +0100

    Add datatype definition for MetaInfo

---
+++ src/Data/Conversion/Problem/Common/MetaInfo.hs
@@ -0,0 +1,44 @@
+-- |
+-- Module      : Data.Conversion.Problem.Common.MetaInfo
+-- Description : MetaInfo type definition
+--
+-- This module contains an extensible 'MetaInfo' type definition.
+module Data.Conversion.Problem.Common.MetaInfo
+  ( -- * Types
+    MetaInfo (..),
+  )
+where
+
+import Prettyprinter (Pretty, pretty, tupled, vsep, (<+>))
+
+-- | The type for additional information about a TRS. Includes keywords for the problem origin,
+-- doi, and arbitrary comments.
+--
+-- As all fields are optional, if no value is given then this is represented by @Nothing@:
+-- an empty entry is represented by @Just ""@ or @Just []@.
+data MetaInfo = MetaInfo
+  { -- | Arbitrary comment(s) that do not fit into the remaining 'MetaInfo' categories. e.g. @["An example TRS"]@
+    comment :: Maybe [String],
+    -- | The doi of the problem if available. e.g. @"10.1007/11805618_6"@
+    doi :: Maybe String,
+    -- | The origin of the problem. e.g. @"COPS #20"@
+    origin :: Maybe String,
+    -- | The individual(s) who submitted the problem. e.g. @["Takahito Aoto","Junichi Yoshida","Yoshihito Toyama"]@
+    submitted :: Maybe [String]
+  }
+  deriving (Eq, Show)
+
+-- | Allow pretty printing 'MetaInfo's
+instance Pretty MetaInfo where
+  pretty (MetaInfo cs ds orig sub) =
+    vsep
+      [ pretty "Comments:  " <+> fromMaybeTuple cs,
+        pretty "Doi:       " <+> pretty ds,
+        pretty "Origin:    " <+> pretty orig,
+        pretty "Submitted: " <+> fromMaybeTuple sub
+      ]
+    where
+      -- Print xs as a comma-separated tuple
+      fromMaybeTuple maybeXs = case maybeXs of
+        Just xs -> tupled (map pretty xs)
+        Nothing -> pretty "No value given"
