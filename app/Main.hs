bfc95166c14c18748841b2ef752665dc9fdca9c0
Jamie Fox <foxjamiek@gmail.com>
2023-03-14 22:29:17 +0100

    Add MSTRS example to Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,61 @@
 module Main (main) where
 
+import Data.Conversion.Parse.ParseMsTrs (parseAriMsTrs)
 import Data.Conversion.Parse.ParseTrs (parseCopsTrs)
+import Data.Conversion.Problem.MsTrs.MsTrs (MsTrs)
 import Data.Conversion.Problem.Trs.Trs (Trs)
+import Data.Conversion.Unparse.UnparseMsTrs (unparseCopsMsTrs)
 import Data.Conversion.Unparse.UnparseTrs (unparseAriTrs)
 import Data.Text (pack)
-import Text.Megaparsec (errorBundlePretty, parse)
+import Prettyprinter (Doc)
+import Text.Megaparsec (eof, errorBundlePretty, parse)
 
 -- | @trs-conversion-exe@ entry point. Can be run by calling
 -- > stack build
 -- > stack exec trs-conversion-exe
+--
+-- Currently contains a simple hard-coded example to illustrate parsing and unparsing functionality.
 main :: IO ()
 main = do
-  putStrLn $ "Parsing example TRS: " ++ input ++ "\n"
-  trs <- parseCops input
-  putStrLn $ "Got result: " ++ show trs ++ "\n"
-  case unparseAriTrs trs of
-    Left err -> ioError $ userError err
-    Right ariTrs -> print ariTrs
+  -- Parse and unparse inputTrs
+  putStrLn $ "Parsing example TRS:\n" ++ inputTrs ++ "\n"
+  trs <- parseCops inputTrs
+  putStrLn $ "Got result:\n" ++ show trs ++ "\n"
+  ariTrs <- unparseIo unparseAriTrs trs
+  putStrLn $ "In ARI format:\n" ++ show ariTrs
+  putStrLn "\n--------\n"
+  -- Parse and unparse inputMsTrs
+  putStrLn $ "Parsing example MSTRS:\n" ++ inputMsTrs ++ "\n"
+  mstrs <- parseAri inputMsTrs
+  putStrLn $ "Got result:\n" ++ show mstrs ++ "\n"
+  copsMsTrs <- unparseIo unparseCopsMsTrs mstrs
+  putStrLn $ "In COPS format:\n" ++ show copsMsTrs
   where
-    input = "(VAR x y)(RULES  f(x,y) -> g(c))(COMMENT A simple example)"
+    inputTrs = "(VAR x y)\n(RULES  f(x,y) -> g(c))\n(COMMENT A simple example)"
+    inputMsTrs =
+      "(format MSTRS)\n\
+      \(sort List)\n\
+      \(sort Nat)\n\
+      \(fun app :sort (List List List))\n\
+      \(fun cons :sort (Nat List List))\n\
+      \(fun nil :sort (List))\n\
+      \(rule (app nil ys) ys)\n\
+      \(rule (app (cons x xs) ys) (cons x (app xs ys)))"
 
 -- | Run 'parseCopsTrs' on an input string and return the resulting 'Trs'
 parseCops :: String -> IO (Trs String String)
-parseCops input = case parse parseCopsTrs "COPS Example" (pack input) of
+parseCops input = case parse (parseCopsTrs <* eof) "COPS TRS Example" (pack input) of
   Left err -> ioError $ userError (errorBundlePretty err)
   Right trs -> return trs
+
+-- | Run 'parseAriMsTrs' on an input string and return the resulting 'MsTrs'
+parseAri :: String -> IO (MsTrs String String String)
+parseAri input = case parse (parseAriMsTrs <* eof) "ARI MSTRS Example" (pack input) of
+  Left err -> ioError $ userError (errorBundlePretty err)
+  Right mstrs -> return mstrs
+
+-- | Takes an unparsing function @up@ as an argument and wraps the result in the IO monad
+unparseIo :: (a -> Either String (Doc a)) -> a -> IO (Doc a)
+unparseIo up input = case up input of
+  Left err -> ioError $ userError err
+  Right copsMsTrs -> return copsMsTrs
