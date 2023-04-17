fb51e0ec22c9fd4ce769ccb68dd04a5a59f0fb9a
Fabian Mitterwallner <fabian99m@gmail.com>
2023-04-17 16:58:06 +0200

    cleanup

--- src/TRSConversion/Parse/ARI/Utils.hs
+++ src/TRSConversion/Parse/ARI/Utils.hs
@@ -5,6 +5,7 @@ module TRSConversion.Parse.ARI.Utils (
   -- * Type
   ARIParser,
   toParser,
+
   -- * Lexing
   lexeme,
   spaces,
@@ -12,13 +13,24 @@ module TRSConversion.Parse.ARI.Utils (
   keyword,
   ident,
   naturalNumber,
+
   -- * Combinators
   sExpr,
   parens,
 ) where
 
 import Data.Text (Text, unpack)
-import Text.Megaparsec (MonadParsec, between, empty, noneOf, notFollowedBy, takeWhile1P, takeWhileP, try, (<?>))
+import Text.Megaparsec (
+  MonadParsec,
+  between,
+  empty,
+  noneOf,
+  notFollowedBy,
+  takeWhile1P,
+  takeWhileP,
+  try,
+  (<?>),
+ )
 import Text.Megaparsec.Char (char, space1, string)
 import qualified Text.Megaparsec.Char.Lexer as L
 
@@ -29,10 +41,19 @@ import TRSConversion.Parse.Utils (Parser)
 
 -- | Type of the COPS parser.
 newtype ARIParser a = ARIParser {toParser :: Parser a}
-  deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadFail, MonadParsec Void Text)
+  deriving
+    ( Functor
+    , Applicative
+    , Alternative
+    , Monad
+    , MonadPlus
+    , MonadFail
+    , MonadParsec Void Text
+    )
 
--- | @'lineComment'@ consumes a line-comment, which starts with a @;@ character
--- and continues until a newline character (the newline character is not consumed).
+{- | @'lineComment'@ consumes a line-comment, which starts with a @;@ character
+and continues until a newline character (the newline character is not consumed).
+-}
 lineComment :: ARIParser ()
 lineComment = do
   _ <- char ';' <* notFollowedBy (string " @")
@@ -58,17 +79,18 @@ that is any string of characters not containing a whitespace, any character in
 -}
 ident :: ARIParser String
 ident =
-  lexeme (unpack <$> takeWhile1P Nothing (\c -> c `notElem` ("(); \t\n\r" :: [Char])))
+  lexeme (unpack <$> takeWhile1P Nothing (`notElem` ("(); \t\n\r" :: [Char])))
     <?> "identifier"
 
 keywordChar :: ARIParser Char
 keywordChar = noneOf ("(); \t\n\r" :: [Char])
 
--- | @'keyword' name@ defines and parses a keyword.
---
--- The parser only succeeds if the keyword is followed by a character in
--- @"(); \t\n\r"@ or a @eof@. This makes sure that we parse full keywords,
--- not only a prefix.
+{- | @'keyword' name@ defines and parses a keyword.
+
+The parser only succeeds if the keyword is followed by a character in
+@"(); \t\n\r"@ or a @eof@. This makes sure that we parse full keywords,
+not only a prefix.
+-}
 keyword :: Text -> ARIParser Text
 keyword word = lexeme (try (string word <* notFollowedBy keywordChar))
 
@@ -76,14 +98,16 @@ keyword word = lexeme (try (string word <* notFollowedBy keywordChar))
 sExpr :: Text -> ARIParser a -> ARIParser a
 sExpr hd = between (try (symbol "(" *> keyword hd)) (symbol ")")
 
--- | @'parens' p@ parses @'('@ followed by @p@ followed by @')'@.
---
--- It also consumes trailing whitespace after the parentheses (but not after @p@).
+{- | @'parens' p@ parses @'('@ followed by @p@ followed by @')'@.
+
+It also consumes trailing whitespace after the parentheses (but not after @p@).
+-}
 parens :: ARIParser a -> ARIParser a
 parens = between (symbol "(") (symbol ")")
 
--- | @'naturalNumber'@ parses a natural number.
---
--- It consumes trailing whitespace
+{- | @'naturalNumber'@ parses a natural number.
+
+It consumes trailing whitespace
+-}
 naturalNumber :: ARIParser Int
 naturalNumber = lexeme L.decimal
--- src/TRSConversion/Parse/COPS/Utils.hs
+++ src/TRSConversion/Parse/COPS/Utils.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -19,8 +20,19 @@ module TRSConversion.Parse.COPS.Utils (
     parens,
 ) where
 
-import Data.Text (Text)
-import Text.Megaparsec (MonadParsec, between, empty, noneOf, notFollowedBy, satisfy, some, try, (<|>))
+import Data.Text (Text, pack)
+import Text.Megaparsec (
+    MonadParsec,
+    between,
+    choice,
+    empty,
+    noneOf,
+    notFollowedBy,
+    satisfy,
+    some,
+    try,
+    (<|>),
+ )
 import Text.Megaparsec.Char (char, space1, string)
 import qualified Text.Megaparsec.Char.Lexer as L
 
@@ -32,7 +44,15 @@ import TRSConversion.Parse.Utils (Parser)
 
 -- | Type of the COPS parser.
 newtype COPSParser a = COPSParser {toParser :: Parser a}
-    deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadFail, MonadParsec Void Text)
+    deriving
+        ( Functor
+        , Applicative
+        , Alternative
+        , Monad
+        , MonadPlus
+        , MonadFail
+        , MonadParsec Void Text
+        )
 
 -- | @'spaces'@ skips any whitespace characters
 spaces :: COPSParser ()
@@ -52,44 +72,62 @@ that is any string of characters not containing a whitespace, any character in
 {(, ), ", |, \ }, and sub-strings "->", "==", "COMMENT", "VAR", "RULES"
 -}
 ident :: COPSParser String
-ident =
-    lexeme
-        ( try . some $
-            ( (char '-' <* (notFollowedBy (char '>') <|> fail "identifiers may not contain the sequence \"->\""))
-                <|> (char '=' <* (notFollowedBy (string "=") <|> fail "identifiers may not contain the sequence \"==\""))
-                <|> (char 'C' <* (notFollowedBy (string "OMMENT") <|> fail "identifiers may not contain the sequence \"COMMENT\""))
-                <|> (char 'V' <* (notFollowedBy (string "AR") <|> fail "identifiers may not contain the sequence \"VAR\""))
-                <|> (char 'R' <* (notFollowedBy (string "ULES") <|> fail "identifiers may not contain the sequence \"RULES\""))
-                <|> satisfy (\c -> not (isSpace c) && c `notElem` ("(),|\\\"" :: [Char]))
-            )
-        )
+ident = lexeme (try . some $ identChar)
+  where
+    failSubseq s =
+        fail ("identifiers may not contain the sequence \"" ++ s ++ "\"")
+
+    -- parses a character as long as it is not part of the sequence
+    charNotSubSeq :: String -> COPSParser Char
+    charNotSubSeq s@(x : xs) =
+        char x <* (notFollowedBy (string (pack xs)) <|> failSubseq s)
+    charNotSubSeq [] = empty
+
+    identChar =
+        choice
+            [ charNotSubSeq "->"
+            , charNotSubSeq "=="
+            , charNotSubSeq "COMMENT"
+            , charNotSubSeq "VAR"
+            , charNotSubSeq "RULES"
+            , satisfy
+                ( \c ->
+                    not (isSpace c)
+                        && c `notElem` ("(),|\\\"" :: [Char])
+                )
+            ]
 
 keywordChar :: COPSParser Char
 keywordChar = noneOf ("(); \t\n\r" :: [Char])
 
--- | @'keyword' name@ defines and parses a keyword.
---
--- The parser only succeeds if the keyword is followed by a character in
--- @"(); \t\n\r"@ or a @eof@. This makes sure that we parse full keywords,
--- not only a prefix.
+{- | @'keyword' name@ defines and parses a keyword.
+
+The parser only succeeds if the keyword is followed by a character in
+@"(); \t\n\r"@ or a @eof@. This makes sure that we parse full keywords,
+not only a prefix.
+-}
 keyword :: Text -> COPSParser Text
 keyword word = lexeme (try (string word <* notFollowedBy keywordChar))
 
--- | @'block' p@ parses a COPS block with the header @name@ and content @p@.
---
--- A COPS block starts with a opening parenthesis, followed by the @name@,
--- it ends with a closing parenthesis. (e.g, "(VAR x y z)" where "x y z" is consumed by p)
+{- | @'block' p@ parses a COPS block with the header @name@ and content @p@.
+
+A COPS block starts with a opening parenthesis, followed by the @name@,
+it ends with a closing parenthesis. (e.g, "(VAR x y z)" where "x y z" is
+consumed by p)
+-}
 block :: Text -> COPSParser a -> COPSParser a
 block hd = between (try (string "(" *> keyword hd)) (symbol ")")
 
--- | @'parens' p@ parses @'('@ followed by @p@ followed by @')'@.
---
--- It also consumes trailing whitespace after the parentheses (but not after @p@).
+{- | @'parens' p@ parses @'('@ followed by @p@ followed by @')'@.
+
+It also consumes trailing whitespace after the parentheses (but not after @p@).
+-}
 parens :: COPSParser a -> COPSParser a
 parens = between (symbol "(") (symbol ")")
 
--- | @'naturalNumber'@ parses a natural number.
---
--- It consumes trailing whitespace
+{- | @'naturalNumber'@ parses a natural number.
+
+It consumes trailing whitespace
+-}
 naturalNumber :: COPSParser Int
 naturalNumber = lexeme L.decimal
