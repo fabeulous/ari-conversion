8aefcb6884694d2e6410df7357c9c3113f1845e5
Fabian Mitterwallner <fabian99m@gmail.com>
2023-10-16 16:11:11 +0200

    check variable conditions for systems
    - for TRS, MSTRS, and CSTRS check both that there are no
      variable left-hand sides and no variables on the right,
      not appearing on the left.
    - for CTRS, CSCTRS and Infeasibility problems on check that
      there are no variable left-hand sides

--- ari-syntax-checker/Main.hs
+++ ari-syntax-checker/Main.hs
@@ -1,13 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import qualified Data.Set as Set
 import qualified Data.Text.IO as Text
+import Prettyprinter (Pretty, vsep)
 import System.Environment (getArgs, getProgName)
 import System.Exit (exitFailure, exitSuccess)
 import System.IO (hPutStrLn, stderr)
 import qualified TRSConversion.Parse.ARI.Problem as ARI
 import qualified TRSConversion.Parse.ARI.Utils as ARI
 import TRSConversion.Parse.Utils (parseIO)
+import qualified TRSConversion.Problem.CSCTrs.CSCTrs as CSCTrs
+import qualified TRSConversion.Problem.CSTrs.CSTrs as CSTrs
+import qualified TRSConversion.Problem.CTrs.CTrs as CTrs
+import qualified TRSConversion.Problem.CTrs.Infeasibility as Inf
+import TRSConversion.Problem.Common.Term (vars)
+import qualified TRSConversion.Problem.MsTrs.MsTrs as MsTrs
+import TRSConversion.Problem.Problem (System (..), system)
 import qualified TRSConversion.Problem.Problem as Prob
+import TRSConversion.Problem.Trs.Trs (Term (..))
+import qualified TRSConversion.Problem.Trs.Trs as Trs
+import TRSConversion.Unparse.CTrs (unparseAriCRules)
+import TRSConversion.Unparse.Problem.Rule (unparseAriRule)
 
 main :: IO ()
 main = do
@@ -22,17 +37,113 @@ main = do
 usage :: String -> String
 usage name = "Usage: " ++ name ++ " FILE"
 
+data Result a = Fail a | Succeed
+    deriving (Eq, Ord, Show)
+
+instance Semigroup (Result a) where
+    Fail a <> _ = Fail a
+    Succeed <> b = b
+
+instance Monoid (Result a) where
+    mappend = (<>)
+    mempty = Succeed
+
 runApp :: FilePath -> IO ()
 runApp fp = do
     fileContent <- Text.readFile fp
     problem <- parseIO (ARI.toParser ARI.parseProblem) fp fileContent
-    putStr $ case Prob.system problem of
-        (Prob.Trs _) -> "trs"
-        (Prob.MSTrs _) -> "mstrs"
-        (Prob.CTrs _) -> "ctrs"
-        (Prob.CSTrs _) -> "cstrs"
-        (Prob.CSCTrs _) -> "csctrs"
-        (Prob.Infeasibility _) -> "infeasibility"
-    putStrLn " successfully parsed"
+    case checkSem (system problem) of
+        Fail err -> do
+            hPutStrLn stderr "ERROR:"
+            hPutStrLn stderr err
+            exitFailure
+        Succeed -> do
+            putStr $ case Prob.system problem of
+                (Prob.Trs _) -> "trs"
+                (Prob.MSTrs _) -> "mstrs"
+                (Prob.CTrs _) -> "ctrs"
+                (Prob.CSTrs _) -> "cstrs"
+                (Prob.CSCTrs _) -> "csctrs"
+                (Prob.Infeasibility _) -> "infeasibility"
+            putStrLn " successfully parsed"
+            exitSuccess
 
-    exitSuccess
+checkSem :: System -> Result String
+checkSem (Trs sys) = checkTrs sys
+checkSem (CTrs sys) = checkCTrs sys
+checkSem (MSTrs sys) = checkMSTrs sys
+checkSem (CSTrs sys) = checkCSTrs sys
+checkSem (CSCTrs sys) = checkCSCTrs sys
+checkSem (Infeasibility sys) = checkInfeasibility sys
+
+--------------------------------------------------------------------------------
+------ TRSs
+
+checkTrs :: (Ord v, Pretty f, Pretty v) => Trs.Trs f v -> Result String
+checkTrs Trs.Trs{Trs.rules = rs} = foldMap (foldMap checkRule) rs
+
+checkRule :: (Ord v, Pretty f, Pretty v) => Trs.Rule f v -> Result String
+checkRule rule@Trs.Rule{Trs.lhs = l, Trs.rhs = r} =
+    case l of
+        Var _ ->
+            Fail
+                . show
+                $ vsep
+                    [ "the rule"
+                    , unparseAriRule 1 rule
+                    , "may not have a variable left-hand side"
+                    ]
+        _
+            | not (rVars `Set.isSubsetOf` lVars) ->
+                Fail
+                    . show
+                    $ vsep
+                        [ "the rule"
+                        , unparseAriRule 1 rule
+                        , "violates the variable condition"
+                        ]
+        _ -> Succeed
+  where
+    lVars = Set.fromList (vars l)
+    rVars = Set.fromList (vars r)
+
+--------------------------------------------------------------------------------
+------ CTRSs
+
+checkCTrs :: (Pretty f, Pretty v) => CTrs.CTrs f v -> Result String
+checkCTrs CTrs.CTrs{CTrs.rules = rs} = foldMap (foldMap checkCRule) rs
+
+checkCRule :: (Pretty f, Pretty v) => CTrs.CRule f v -> Result String
+checkCRule rule@CTrs.CRule{CTrs.lhs = l} =
+    case l of
+        Var _ ->
+            Fail
+                . show
+                $ vsep
+                    [ "the rule"
+                    , unparseAriCRules 1 [rule]
+                    , "may not have a variable left-hand side"
+                    ]
+        _ -> Succeed
+
+--------------------------------------------------------------------------------
+------ MSTrs
+
+checkMSTrs :: MsTrs.MsTrs String String String -> Result String
+checkMSTrs MsTrs.MsTrs{MsTrs.rules = rs} = foldMap (foldMap checkRule) rs
+
+--------------------------------------------------------------------------------
+------ CSTrs
+checkCSTrs :: CSTrs.CSTrs String String -> Result String
+checkCSTrs CSTrs.CSTrs{CSTrs.rules = rs} = foldMap (foldMap checkRule) rs
+
+--------------------------------------------------------------------------------
+------ CSCTrs
+checkCSCTrs :: CSCTrs.CSCTrs String String -> Result String
+checkCSCTrs CSCTrs.CSCTrs{CSCTrs.ctrs = ctrs} = checkCTrs ctrs
+
+--------------------------------------------------------------------------------
+------ Infeasibility
+
+checkInfeasibility :: Inf.Infeasibility String String -> Result String
+checkInfeasibility Inf.Infeasibility{Inf.ctrs = ctrs} = checkCTrs ctrs
