5115890a402b190d06d9dc4a517f1c13dd16b6c5
Fabian Mitterwallner <fabian99m@gmail.com>
2023-11-08 15:50:34 +0100

    cleanup

--- src/TRSConversion/Formats/CPF3/Unparse/Problem.hs
+++ src/TRSConversion/Formats/CPF3/Unparse/Problem.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module TRSConversion.Formats.CPF3.Unparse.Problem (
-    problemToXML,
+  problemToXML,
 )
 where
 
@@ -21,23 +21,23 @@ import TRSConversion.Problem.Trs.Trs (Rule (..), Sig (..), Term (..), Trs (..), 
 
 problemToXML :: (Pretty f, Pretty v) => Prob.Problem f v s -> Document
 problemToXML prob = Document (Prologue [] Nothing []) root []
-  where
-    root = Element "input" Map.empty $ case Prob.system prob of
-        Prob.Trs trss -> trssToNodes trss
-        Prob.Infeasibility inf -> infToNodes inf
-        _ -> error "CPF3 currently only supports TRSs, two-TRSs, and infeasibility problems"
+ where
+  root = Element "input" Map.empty $ case Prob.system prob of
+    Prob.Trs trss -> trssToNodes trss
+    Prob.Infeasibility inf -> infToNodes inf
+    _ -> error "CPF3 currently only supports TRSs, two-TRSs, and infeasibility problems"
 
 -- One or two trss
 
 trssToNodes :: (Pretty f, Pretty v) => Trs f v -> [Node]
 trssToNodes trs@Trs{signature = sig, rules = rs}
-    | numSystems trs == 2 = twoTrsToNodes sig (rs IntMap.! 1) (rs IntMap.! 2)
-    | numSystems trs == 1 = singleTrsToNodes sig (rs IntMap.! 1)
-    | otherwise = error "CPF3 does not support output of problems with more than 2 trss"
+  | numSystems trs == 2 = twoTrsToNodes sig (rs IntMap.! 1) (rs IntMap.! 2)
+  | numSystems trs == 1 = singleTrsToNodes sig (rs IntMap.! 1)
+  | otherwise = error "CPF3 does not support output of problems with more than 2 trss"
 
 singleTrsToNodes :: (Pretty f, Pretty v) => TrsSig f -> [Rule f v] -> [Node]
 singleTrsToNodes sig rs =
-    [xml|
+  [xml|
 <trsWithSignature>
   ^{signatureToNodes sig}
   <trs>
@@ -48,7 +48,7 @@ singleTrsToNodes sig rs =
 
 twoTrsToNodes :: (Pretty f, Pretty v) => TrsSig f -> [Rule f v] -> [Rule f v] -> [Node]
 twoTrsToNodes sig r1 r2 =
-    [xml|
+  [xml|
 <input>
   <twoTrsWithSignature>
     ^{signatureToNodes sig}
@@ -64,35 +64,28 @@ twoTrsToNodes sig r1 r2 =
 
 signatureToNodes :: (Pretty f) => TrsSig f -> [Node]
 signatureToNodes (FunSig sig) =
-    [xml|
+  [xml|
 <signature>
   $forall Sig f n <- sig
     <symbol>
-      <name>
-        #{pack (show (pretty f))}
-      <arity>
-        #{pack (show n)}
+      <name>#{pack (show (pretty f))}
+      <arity>#{pack (show n)}
 |]
 
 ruleToNodes :: (Pretty f, Pretty v) => Rule f v -> [Node]
 ruleToNodes Rule{lhs = l, rhs = r} =
-    [xml|
+  [xml|
 <rule>
   ^{termToNodes l}
   ^{termToNodes r}
 |]
 
 termToNodes :: (Pretty f, Pretty v) => Term f v -> [Node]
-termToNodes (Var v) =
-    [xml|
-<var>
-  #{pack (show (pretty v))}
-|]
+termToNodes (Var v) = [xml| <var>#{pack (show (pretty v))} |]
 termToNodes (Fun f ts) =
-    [xml|
+  [xml|
 <funapp>
-  <name>
-    #{pack (show (pretty f))}
+  <name>#{pack (show (pretty f))}
   $forall t <- ts
     ^{termToNodes t}
 |]
@@ -101,7 +94,7 @@ termToNodes (Fun f ts) =
 
 infToNodes :: (Pretty f, Pretty v) => Infeasibility f v -> [Node]
 infToNodes inf =
-    [xml|
+  [xml|
 <input>
   ^{ctrsToNodes (ctrs inf)}
   ^{queryToNodes (query inf)}
@@ -109,11 +102,11 @@ infToNodes inf =
 
 ctrsToNodes :: (Pretty f, Pretty v) => CTrs f v -> [Node]
 ctrsToNodes sys
-    | CTrs.conditionType sys /= CTrs.Oriented = error "CPF3 only supports ORIENTED ctrss"
-    | CTrs.numSystems sys /= 1 = error "CPF3 only supports problems with a single CTRS"
-    | otherwise =
-        let rs = CTrs.rules sys IntMap.! 1
-         in [xml|
+  | CTrs.conditionType sys /= CTrs.Oriented = error "CPF3 only supports ORIENTED ctrss"
+  | CTrs.numSystems sys /= 1 = error "CPF3 only supports problems with a single CTRS"
+  | otherwise =
+      let rs = CTrs.rules sys IntMap.! 1
+       in [xml|
 <ctrs>
   <conditionType>
     <oriented>
@@ -124,7 +117,7 @@ ctrsToNodes sys
 
 cRuleToNodes :: (Pretty f, Pretty v) => CTrs.CRule f v -> [Node]
 cRuleToNodes CTrs.CRule{CTrs.lhs = l, CTrs.rhs = r, CTrs.conditions = conds} =
-    [xml|
+  [xml|
 <rule>
   ^{termToNodes l}
   ^{termToNodes r}
@@ -135,7 +128,7 @@ cRuleToNodes CTrs.CRule{CTrs.lhs = l, CTrs.rhs = r, CTrs.conditions = conds} =
 
 conditionToNodes :: (Pretty f, Pretty v) => CTrs.Condition f v -> [Node]
 conditionToNodes (l :== r) =
-    [xml|
+  [xml|
 <condition>
   ^{termToNodes l}
   ^{termToNodes r}
@@ -143,15 +136,15 @@ conditionToNodes (l :== r) =
 
 queryToNodes :: (Pretty f, Pretty v) => [Condition f v] -> [Node]
 queryToNodes conditions =
-    [xml|
-<probs>
+  [xml|
+<infeasibilityQuery>
   <rules>
     $forall cond <- conditions
       ^{queryCond cond}
 |]
-  where
-    queryCond (l :== r) =
-        [xml|
+ where
+  queryCond (l :== r) =
+    [xml|
 <rule>
   ^{termToNodes l}
   ^{termToNodes r}
